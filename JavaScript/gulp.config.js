8097345fcfbd0d5e91989c7ff98f3e2c4c8e3fff
oshalygin@gmail.com <oshalygin@gmail.com>
2015-12-23 21:12:43 -0800

    fixed gupfile placeholders

---
+++ JavaScript/gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+
+    var publicDirectory = "./wwwroot/";
+    // var bowerJson = require("./bower.json");
+
+
+    var config = {
+        wwwroot: publicDirectory
+    };
+
+    return config;
+}
