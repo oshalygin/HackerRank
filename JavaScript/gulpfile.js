f4b183863147778e0ecb2785f12d088e7b613290
oshalygin@gmail.com <oshalygin@gmail.com>
2015-12-24 22:52:18 -0800

    a few changes to the gulp file and a few more configurations

--- JavaScript/gulpfile.js
+++ JavaScript/gulpfile.js
@@ -7,3 +7,18 @@ var $ = require("gulp-load-plugins")({
 var Server = require("karma").Server;
 
 
+
+
+
+
+function log(msg) {
+    if (typeof (msg) === "object") {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
