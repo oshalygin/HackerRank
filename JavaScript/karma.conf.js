48b633d356701d6390a3cb71ee54bb1826d7554b
oshalygin@gmail.com <oshalygin@gmail.com>
2015-12-26 22:03:25 -0800

    added some dependencies to the karma config file

--- JavaScript/karma.conf.js
+++ JavaScript/karma.conf.js
@@ -1,4 +1,3 @@
-
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -9,8 +8,17 @@ module.exports = function (config) {
         exclude: [
         ],
         reporters: ['mocha'],
-
+        colors: true,
+        logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['PhantomJS']
+        browsers: ['PhantomJS'],
+        plugins: [
+            'karma-jasmine',
+            'karma-mocha-reporter',
+            "karma-phantomjs-launcher"
+
+        ]
+
     });
 };
+
