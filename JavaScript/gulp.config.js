15018e24ab8893fee9227f5584e70b2d410c1a72
oshalygin@gmail.com <oshalygin@gmail.com>
2016-01-07 20:48:12 -0800

    moving karma config into the gulp config file

--- JavaScript/gulp.config.js
+++ JavaScript/gulp.config.js
@@ -1,12 +1,34 @@
-module.exports = function () {
+"use strict";
 
+module.exports = () => {
 
-    var solutionsJavaScriptFiles = "solutions/**/*.js";
-    var excludeNodeModulesDirectory = "!node_modules/**";
-    var karmaConfig = __dirname + "/karma.conf.js";
-    var solutionsPath = "./solutions";
+    let solutionsJavaScriptFiles = "solutions/**/*.js";
+    let excludeNodeModulesDirectory = "!node_modules/**";
+    let solutionsPath = "./solutions";
 
-    var config = {
+    let karmaConfig = function (config) {
+        config.set({
+            basePath: '',
+            frameworks: ['jasmine'],
+            files: [
+                { pattern: 'solutions/**/*.js' }
+            ],
+            exclude: [
+            ],
+            reporters: ['mocha'],
+            colors: true,
+            logLevel: config.LOG_INFO,
+            autoWatch: false,
+            browsers: ['PhantomJS'],
+            plugins: [
+                'karma-jasmine',
+                'karma-mocha-reporter',
+                "karma-phantomjs-launcher"
+            ]
+        });
+    };
+
+    let config = {
         solutionsJavaScriptFiles: solutionsJavaScriptFiles,
         excludeNodeModulesDirectory: excludeNodeModulesDirectory,
         karmaConfiguration: karmaConfig,
